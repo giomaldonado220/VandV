26393f3c37b1f003a3a1872110c44a31ef54b689
DIGamboa33 <derekgamboa33@gmail.com>
2025-02-01 13:21:31 -0700

    Update AvlTreeTest.java

--- src/test/java/org/example/AvlTreeTest.java
+++ src/test/java/org/example/AvlTreeTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AvlTreeTest {
-
+    private AvlTree tree;
     @BeforeEach
     public void setUp(){
         tree = new AvlTree();
