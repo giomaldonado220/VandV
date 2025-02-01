996052a8ccb33df1863d38e6b998632ffce458a4
DIGamboa33 <derekgamboa33@gmail.com>
2025-02-01 13:20:47 -0700

    Update AvlTreeTest.java

--- src/test/java/org/example/AvlTreeTest.java
+++ src/test/java/org/example/AvlTreeTest.java
@@ -1,7 +1,7 @@
 package org.example;
-
+import java.util.Random;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AvlTreeTest {
@@ -23,4 +23,42 @@ class AvlTreeTest {
 
         assertTrue(tree.repOK_Structure(tree)); // Checks AVL balance property
     }
+
+    @Test
+    public void testRandomInsertions(){
+        Random random = new random(30);
+        int numInsertions = 30;
+        int min = -100;
+        int max = 100;
+
+        for(int i = 0; i < numInsertions; i++){
+            int randomInt = random.nextInt();
+            tree.insert(avlTree, randomInt);
+        }
+    }
+
+    public void testInsertions(){
+        tree.insert(tree, 50);
+        tree.insert(tree, 40);
+        tree.insert(tree, 45);
+        tree.insert(tree, 60);
+        tree.insert(tree, 70);
+        tree.insert(tree, 30);
+        tree.insert(tree, 35);
+        tree.insert(tree, 80);
+        tree.insert(tree, 5);
+
+        assertTrue(tree.repOK_Structure(tree));
+
+        int numNodes = 9;
+        int H
+        int height = (int)(Math.log(numNodes)/Math.log(2));
+
+        assertTrue(tree.height == height);
+        assertEquals(45, tree.element);
+    }
+        
+        
+        
+        
 }
